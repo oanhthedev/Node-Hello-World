ecdd6ba6beee8f7e41e1f1c3b953845e7c35f1e0
oanhcoder@gmail.com <oanh@OanhLeMacPro.local>
2023-12-07 12:57:43 +0700

    node server for 'Hello World'

---
+++ hello-world.js
@@ -0,0 +1,18 @@
+const http = require("node:http");
+
+
+const hostname = "127.0.0.1"; 
+const port = "3000"; 
+
+const server = http.createServer(function(request,response)
+{
+    response.writeHead(200,{
+        "Content-Type": "text/plain", } );
+    response.end("Hello World!"); 
+}); 
+
+server.listen(port,hostname,() => {
+    console.log("Server is running at http://",hostname,":",port,"/");
+} );
+
+ 
